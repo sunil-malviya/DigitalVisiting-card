7dbe37e7c3906128a681fdd8a23d08bb1ca69bbd
sunil malviya <sunilkumar.malviya@vastraapp.com>
2022-08-26 22:26:50 +0530

    Vasta Visiting Card

--- app.js
+++ app.js
@@ -36,14 +36,15 @@
             
 
            app.get('/',(req,res) =>{
-              res.render('userReg');
+              res.render('homePage');
              })
         
-           app.post('/pdf',upload.single('logo'),async(req,res) =>{
+           app.post('/pdf-view',upload.single('logo'),async(req,res) =>{
               let user = req.body;
-              user.address = user.address.toString().trim() || "";
-               console.log("user",user);
-            //   console.log("user>>>>>",user.address.length);
+               // user.address = user.address.toString().trim() || "";
+               // console.log("user",user);
+          
+            //   console.log("user>>>>>",user.address.length);                   
               let image  = req.file.filename;
               let logo   =   await imageToBase64(`public/iamges/${image}`);// Path to the image
               let insta  =   await imageToBase64("public/iamges/insta.png");// Path to the image
@@ -52,9 +53,38 @@
               let call   =   await imageToBase64("public/iamges/telephone-call.png");// Path to the image
               let mail   =   await imageToBase64("public/iamges/gmail.png");// Path to the image
               let weblogo=   await imageToBase64("public/iamges/world-wide-web.png");// Path to the image
+             
+                 res.render('downloadPdf',{user,
+                                logo,
+                                insta,
+                                twitter,
+                                map,
+                                call,
+                                mail,
+                                weblogo
+                            });
+                    fs.unlinkSync(`public/iamges/${image}`);
+       })
+
+             app.post('/pdf',upload.single('logo'),async(req,res) =>{
+              let user = req.body;
+            //   user.address = user.address.toString().trim() || "";
+               // console.log("user",user);
+          
+              let addLine = user.lineHeight;
+              let cline = user.clineHeight;             
+            //   let image  = req.file.filename;
+            //   let logo   =   await imageToBase64(`public/iamges/${image}`);
+              let insta  =   await imageToBase64("public/iamges/insta.png");// Path to the image
+              let twitter=   await imageToBase64("public/iamges/twitter.png");// Path to the image
+              let map    =   await imageToBase64("public/iamges/google-maps.png");// Path to the image
+              let call   =   await imageToBase64("public/iamges/telephone-call.png");// Path to the image
+              let mail   =   await imageToBase64("public/iamges/gmail.png");// Path to the image
+              let weblogo=   await imageToBase64("public/iamges/world-wide-web.png");// Path to the image
+              let logo  =   user.logo;
               const  pdfTemplate = await ejs.renderFile(
                 path.join(__dirname, './views/index.ejs'),
-                {
+                 {
                       user,
                       logo,
                       insta,
@@ -63,15 +93,23 @@
                       call,
                       mail,
                       weblogo
-                })   
-                 
-                 
-               const options = { "width":"216mm", "height":"425mm",}
+                 })   
+
+                 let height =  370+((addLine-1)*11);
+                 if(cline>1){
+                     if(cline>2){ 
+                     height = (height + 35);}
+                     height = (height + 22)
+                  }
+               const options = { "width":"217mm", "height":`${height}mm`,}
               
                pdf.create( pdfTemplate,options).toFile(path.join(__dirname, 'businesscard.pdf'), (err, result) => {
                     if (!err) {
-                     console.log(result)
+                     // console.log(result)
                      }
+                           
+                        //  console.log(pdfTemplate);
+               
                             res.sendFile( path.join(__dirname, 'businesscard.pdf'))
                            //      res.render('index',{user,
                            //      logo,
@@ -83,7 +121,7 @@
                            //      weblogo
                            //  });
                          })
-                           fs.unlinkSync(`public/iamges/${image}`);
+                           
                        })
-
+          
        app.listen(PORT, () => console.log(`Server Running at port ${PORT}`));
